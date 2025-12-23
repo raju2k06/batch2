fa3af1344803771fb2b1155a58c9b270f62c1a2d
raju2k06 <vigneshwararajumg9.tews@gmail.com>
2025-12-23 12:04:18 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+char first[50],last[50],full[100];
+int i = 0, j = 0, length = 0;
+printf("Enter first name :");
+scanf("%s",first);
+printf("Enter the last name :");
+scanf("%s",last);
+while(first[i] != '\0')
+{
+full[length++] = first[i++];
+}
+full[length++] = ' ';
+while (last[j] != '\0')
+{
+full[length++] = last[j++];
+}
+full [length] = '\0';
+printf("Full name : %s \n",full);
+printf("Length : %d \n",length);
+if(length > 20)
+printf("Name too long for screen\n");
+else
+printf("Name fits the screen \n");
+return 0;
+}
